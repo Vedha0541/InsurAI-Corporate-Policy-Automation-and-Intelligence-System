34fe6e2d7298d79691bd5ff441cff7e6eec7ebd9
VEDHA KAVYA SUDHA PERNI <152948573+Vedha0541@users.noreply.github.com>
2025-09-17 20:45:22 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,23 +3,30 @@ import SignupPage from "./pages/SignupPage";
 import LoginPage from "./pages/LoginPage";
 import HomePage from "./pages/HomePage";
 import Dashboard from "./pages/Dashboard";
-import Policies from "./pages/Policies";
-import Verify from "./pages/Verify";
+import PoliciesPage from "./pages/Policies";
+import ApplicationFormPage from "./pages/ApplicationFormPage";
+import VerifyPage from "./pages/Verify";
+import ApplicationsPage from "./pages/ApplicationsPage";
+import AdminDashboardPage from "./pages/AdminDashboardPage";
 
 function App() {
   return (
     <Router>
+      {" "}
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignupPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-        <Route path="/policies" element={<Policies />} />
-        <Route path="/verify" element={<Verify />} />
-        <Route path="*" element={<HomePage />} /> {/* Default to home */}
-      </Routes>
+        {" "}
+        <Route path="/" element={<HomePage />} />{" "}
+        <Route path="/signup" element={<SignupPage />} />{" "}
+        <Route path="/login" element={<LoginPage />} />{" "}
+        <Route path="/dashboard" element={<Dashboard />} />{" "}
+        <Route path="/policies" element={<PoliciesPage />} />{" "}
+        <Route path="/apply/:id" element={<ApplicationFormPage />} />{" "}
+        <Route path="/verify/" element={<VerifyPage />} />{" "}
+        <Route path="/applications" element={<ApplicationsPage />} />{" "}
+        <Route path="/admin/dashboard" element={<AdminDashboardPage />} />{" "}
+        <Route path="*" element={<HomePage />} />{" "}
+      </Routes>{" "}
     </Router>
   );
 }
-
 export default App;
